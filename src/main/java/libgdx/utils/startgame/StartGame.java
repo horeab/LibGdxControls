014521916864ee7de21a90d7aea0c748156b7998
Horea <horea.bucerzan@gmail.com>
2020-01-13 22:05:38 +0200

    balloon

--- src/main/java/libgdx/utils/startgame/StartGame.java
+++ src/main/java/libgdx/utils/startgame/StartGame.java
@@ -14,14 +14,16 @@ public class StartGame {
     private static final Pair<Integer, Integer> e = new ImmutablePair<>(789, 480);
     private static final Pair<Integer, Integer> r = new ImmutablePair<>(320, 240);
     private static final Pair<Integer, Integer> t = new ImmutablePair<>(853, 480);
-    //landscape
     private static final Pair<Integer, Integer> s = new ImmutablePair<>(480, 853);
     private static final Pair<Integer, Integer> a = new ImmutablePair<>(853, 1480);
     private static final Pair<Integer, Integer> x = new ImmutablePair<>(700, 300);
 
     public static void main(Game game, String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        Pair<Integer, Integer> V = t;
+        //landscape
+        Pair<Integer, Integer> V = s;
+        //portrait
+//        Pair<Integer, Integer> V = t;
         cfg.height = V.getLeft();
         cfg.width = V.getRight();
         new LwjglApplication(game, cfg);
