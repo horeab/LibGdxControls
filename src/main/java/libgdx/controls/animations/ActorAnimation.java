1673c577b9965ae54718d2485d3b39328cc5c3ab
horeab <horea.bucerzan@gmail.com>
2020-11-12 00:27:37 +0200

    moved package

--- src/main/java/libgdx/controls/animations/ActorAnimation.java
+++ src/main/java/libgdx/controls/animations/ActorAnimation.java
@@ -141,4 +141,34 @@ public class ActorAnimation {
         screen.addActor(table);
     }
 
+    public void createScrollingBackground(Res backgr) {
+        int scrollSpeed = 5;
+        Image image1 = GraphicUtils.getImage(backgr);
+        image1.setY(0);
+        image1.setFillParent(true);
+        Image image2 = GraphicUtils.getImage(backgr);
+        image2.setY(-ScreenDimensionsManager.getScreenHeight());
+        image2.setFillParent(true);
+        screen.addActor(image1);
+        screen.addActor(image2);
+        animateMoveUp(scrollSpeed, image1);
+        animateMoveUp(scrollSpeed, image2);
+    }
+
+    private void animateMoveUp(final float amount, final Actor actor) {
+        RunnableAction run = new RunnableAction();
+        run.setRunnable(new ScreenRunnable(screen) {
+            @Override
+            public void executeOperations() {
+                animateMoveUp(amount, actor);
+                int screenHeight = ScreenDimensionsManager.getScreenHeight();
+                if (actor.getY() > screenHeight) {
+                    actor.setY(-screenHeight);
+                }
+            }
+        });
+        float duration = 0.5f;
+        actor.addAction(Actions.sequence(Actions.moveBy(0, amount, duration), run));
+    }
+
 }
